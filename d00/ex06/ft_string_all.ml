d42a149b631cc8a8558505a15851a169377cb939
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-10-02 18:20:19 +0200

    ex06

---
+++ d00/ex06/ft_string_all.ml
@@ -0,0 +1,34 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   ft_string_all.ml                                   :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2017/10/02 17:21:49 by adubedat          #+#    #+#             *)
+(*   Updated: 2017/10/02 18:19:19 by adubedat         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let is_digit c =
+    c >= '0' && c <= '9'
+
+let ft_string_all (check: char -> bool) str =
+    let len = String.length str in
+    let rec loop str n =
+        if n = len then
+            true
+        else if is_digit (String.get str n) then
+            loop str (n + 1)
+        else
+            false
+    in
+    loop str 0
+
+let main () =
+    print_endline (string_of_bool(ft_string_all is_digit "0123456789"));
+    print_endline (string_of_bool(ft_string_all is_digit "0123456789B"));
+    print_endline (string_of_bool(ft_string_all is_digit "A0123456789"));
+    print_endline (string_of_bool(ft_string_all is_digit "01234*56789"))
+
+let () = main ()
