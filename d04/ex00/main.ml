06758f67b1e618f9c5cfde0228f488f72483dd7b
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-10-06 18:42:56 +0200

    modif main ex00

--- d04/ex00/main.ml
+++ d04/ex00/main.ml
@@ -6,25 +6,19 @@
 (*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2017/10/06 18:10:36 by adubedat          #+#    #+#             *)
-(*   Updated: 2017/10/06 18:23:24 by adubedat         ###   ########.fr       *)
+(*   Updated: 2017/10/06 18:42:25 by adubedat         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let rec print_list lst =
     match lst with
-        | head :: tail when head = Color.Spade -> print_endline (Color.toString head); print_list tail
-        | head :: tail when head = Color.Heart -> print_endline (Color.toString head); print_list tail
-        | head :: tail when head = Color.Diamond -> print_endline (Color.toString head); print_list tail
-        | head :: tail when head = Color.Club -> print_endline (Color.toString head); print_list tail
-        | [] | _ -> ()
+        | head :: tail -> print_endline (Color.toString head); print_list tail
+        | [] -> ()
 
 let rec print_list_verbose lst =
     match lst with
-        | head :: tail when head = Color.Spade -> print_endline (Color.toStringVerbose head); print_list_verbose tail
-        | head :: tail when head = Color.Heart -> print_endline (Color.toStringVerbose head); print_list_verbose tail
-        | head :: tail when head = Color.Diamond -> print_endline (Color.toStringVerbose head); print_list_verbose tail
-        | head :: tail when head = Color.Club -> print_endline (Color.toStringVerbose head); print_list_verbose tail
-        | [] | _ -> ()
+        | head :: tail -> print_endline (Color.toStringVerbose head); print_list_verbose tail
+        | [] -> ()
 
 let () =
     print_list Color.all;
