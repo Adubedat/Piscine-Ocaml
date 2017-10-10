a66f805032abcb77d85e1a9ee8491cb78c5ff2ea
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-10-10 15:46:59 +0200

    ex01.ml

---
+++ d06/ex01/ex01.ml
@@ -0,0 +1,36 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   ex01.ml                                            :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2017/10/10 14:54:48 by adubedat          #+#    #+#             *)
+(*   Updated: 2017/10/10 15:45:16 by adubedat         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+module StringHash =
+    struct
+        type t = string
+        let equal x y = x = y
+        let hash (str: t) =
+            let len = String.length str in
+            if len = 0 then 7 else
+            begin
+                let rec loop c hash count =
+                    if count = (len - 1) then hash
+                    else loop (String.get str (count + 1)) ((int_of_char c) + 31 * hash) (count + 1)
+                in
+                loop (String.get str 0) 7 0
+            end
+    end
+
+module StringHashtbl = Hashtbl.Make(StringHash)
+
+let () =
+    let ht = StringHashtbl.create 5 in
+    let values = [ "Hello"; "world"; "42"; "Ocaml"; "H" ] in
+    let pairs = List.map (fun s -> (s, String.length s)) values in
+    List.iter (fun (k,v) -> StringHashtbl.add ht k v) pairs;
+    StringHashtbl.iter (fun k v -> Printf.printf "k = \"%s\", v = %d\n" k v) ht
