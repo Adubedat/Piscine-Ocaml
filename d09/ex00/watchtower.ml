155d33101be4245cbf1de7d8b02a8b8df3f5629c
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-10-14 16:28:48 +0200

    ex00

--- d09/ex00/watchtower.ml
+++ d09/ex00/watchtower.ml
@@ -6,7 +6,7 @@
 (*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2017/10/14 15:52:10 by adubedat          #+#    #+#             *)
-(*   Updated: 2017/10/14 16:17:14 by adubedat         ###   ########.fr       *)
+(*   Updated: 2017/10/14 16:28:32 by adubedat         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -16,8 +16,8 @@ struct
     
     let (zero:hour) = 12
 
-    let add (x:hour) (y:hour) = (x + y) mod zero
+    let add (x:hour) (y:hour) : hour = (x + y) mod zero
 
-    let sub (x:hour) (y:hour) = add zero (x - y)
+    let sub (x:hour) (y:hour) : hour = add zero (x - y)
 
 end
